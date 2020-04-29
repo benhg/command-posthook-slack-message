a81ec65f54d5f30f693d620ae7e98b562b2d4957
Ben Glick <glick@glick.cloud>
2020-04-29 12:21:50 -0700

    weirdly, it works

--- execute_notify.py
+++ execute_notify.py
@@ -6,12 +6,12 @@ execute_notify.py
 Execute a command and get a slack notification when it's done
 """
 
-config = {
-	"slack_webhook_link": "link"
-}
-
+import subprocess
 import datetime
-import json
+
+config = {
+	"slack_webhook_link": "link here"
+}
 
 def generate_slack_message_command(input_command, cmd_name="None"):
 	message = f"Command `{input_command}` with name `{cmd_name}` finished at {str(datetime.datetime.now())}"
@@ -19,6 +19,19 @@ def generate_slack_message_command(input_command, cmd_name="None"):
 	send_slack_command = """python3 -c 'import datetime;import json;import requests; requests.post("%s", headers={"Content-type": "application/json"}, data=json.dumps({"text": "%s"}))'""" % (slack_webhook_link, message)
 	return send_slack_command
 
+def run_command_preserve_output(command, cmd_name="None"):
+	r_val = subprocess.Popen(command, shell=True)
+	output, errors = r_val.communicate()
+	slack_send = generate_slack_message_command(command, cmd_name)
+	r_val = subprocess.Popen(slack_send, shell=True)
+	output, errors = r_val.communicate()
 
 
-print(generate_slack_message_command("ls"))
+def generate_bashscript():
+	pass
+
+def submit_to_cluster():
+	pass
+
+
+run_command_preserve_output("sleep 5")
